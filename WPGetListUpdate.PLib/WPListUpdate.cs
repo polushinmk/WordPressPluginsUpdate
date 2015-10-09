87b98796b5166f6824e52a636b842ee116a97f91
polushinmk <polushin@kell.ru>
2015-10-09 09:33:08 +0300

    Initial

---
+++ WPGetListUpdate.PLib/WPListUpdate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPGetListUpdate.PLib.Models;
+using System.IO;
+
+using System.Net;
+
+namespace WPGetListUpdate.PLib
+{
+    public class WPListUpdate
+    {
+        public WPSite Site { get; private set; }
+        public string PathToScripts { get; set; }
+        public WPListUpdate(WPSite site) : this(site, "/wp_update.php") { }
+
+        public WPListUpdate(WPSite site, string pathToScript)
+        {
+            if (!TestURL(site.SiteURi + pathToScript))
+            {
+                throw new ArgumentOutOfRangeException("pathToScript", "Указан неверный путь");
+            }
+            else
+            {
+                this.Site = site;
+                this.PathToScripts = pathToScript;
+            }
+
+        }
+
+        public bool TestURL(string url)
+        {
+            try
+            {
+                HttpWebRequest wbReq = WebRequest.CreateHttp(url);
+                WebResponse wbResp = wbReq.GetResponseAsync().Result;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        public async Task<List<string>> GetPluginsWithUpdate()
+        {
+            HttpWebRequest wbReq = WebRequest.CreateHttp(this.Site.SiteURi + this.PathToScripts);
+            WebResponse wbResp = await wbReq.GetResponseAsync();
+            StreamReader sr = new StreamReader(wbResp.GetResponseStream());
+            string plugins = await sr.ReadToEndAsync();
+            return plugins.Split(',').ToList();
+        }
+    }
+}
---
+++ WPGetListUpdate.Tests/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPGetListUpdate.PLib;
+using WPGetListUpdate.PLib.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace WPGetListUpdate.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public async Task TestWPGetListUpdate()
+        {
+            WPSite site = new WPSite("Максимум", "http://maximum-kirov.ru/");
+            WPListUpdate wplu = new WPListUpdate(site,"wp_update.php");
+            List<string> res = await wplu.GetPluginsWithUpdate();
+            Assert.IsNotNull(res);
+        }
+        [TestMethod]
+        public void TestURi()
+        {
+            HttpWebRequest wbReq = WebRequest.CreateHttp("http://maximum-kirov.ru/dfgdgfdgdfgfdg");
+            WebResponse wbResp = wbReq.GetResponseAsync().Result;
+            string res = (new StreamReader(wbResp.GetResponseStream())).ReadToEndAsync().Result;
+            Assert.IsTrue(true);
+        }
+    }
+}
